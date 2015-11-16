dee1875d0e4ee3672a83d2631755c762c25eabff
Muhammad Fawwaz <fawwaz@groventure.com>
2015-11-16 16:11:42 +0800

    fix security csv file

--- gv_reseller/__openerp__.py
+++ gv_reseller/__openerp__.py
@@ -3,20 +3,10 @@
     'name': "GV Reseller",
 
     'summary': """
-        For resellers to sell Gronex Products
+        Key focus for Gronex Reseller Portal is for convenience and easy communication between resellers, ops and It implementation team. Its to minimize miscommunication and allow sales teams to focus on doing sales while making it an easy process to lay out to ops on the incoming projects.
     """,
 
     'description': """
-    Key focus for Gronex Reseller Portal is for convenience and easy communication between resellers, ops and It implementation team. Its to minimize miscommunication and allow sales teams to focus on doing sales while making it an easy process to lay out to ops on the incoming projects.
-    
-    Functions:
-    Allow resellers to download collateral
-    To provide instant access to resellers for all necessary tools to do their sales smoothly
-    Allow resellers to send order to ops in 1 simple step
-    To provide comprehensive information for resellers for Product Information
-    To adhere to assist Resellers’ help request electronically
-    To allow bookkeeping for resellers to refer for history, past projects and account details
-    Allow access to demo according to permission levels for resellers
 
     """,
 
@@ -34,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+#         'security/ir.model.access.csv',
         'templates.xml',
         'module.xml',
     ],
