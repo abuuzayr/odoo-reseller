33f0802257478d993580752b5853e71d07852910
Muhammad Fawwaz <fawwaz@groventure.com>
2015-11-20 15:40:04 +0800

    add backup files cos gronex git reference in this vm is corrupted

---
+++ gv_limit_users/limit.py
@@ -0,0 +1,56 @@
+from openerp import fields, models, api, exceptions, osv
+
+
+class gv_user_limit(models.Model):
+    """
+
+    """
+    _inherit="res.users"
+
+    def _limit_users(self):          
+        if not self.share:
+            if self.active:
+                # get total number of employees. Need to reduce the value by one because of superadmin account.
+                totalNumberOfEmployees = len(self.env['res.users'].search([('active', '=', True), ('share', '=', False)])) -1
+                userLimit = self.get_user_limit()
+                print userLimit
+                if totalNumberOfEmployees > userLimit:
+                    errorMsg = "You have reached the maximum number of employees allowable. Max: " + str(userLimit) + " Current: " + str(totalNumberOfEmployees)
+                    raise exceptions.ValidationError(errorMsg)
+                
+    def get_user_limit(self):
+        config_array = self.env['gv.config.settings'].search([], limit=1, order='id DESC')
+        if config_array:
+            return config_array[0].userLimit
+        return 0
+
+    @api.multi
+    def write(self, values):
+        self._limit_users()
+        return super(gv_user_limit, self).write(values)
+    
+    @api.one
+    def create(self, values):
+        self._limit_users()
+        return super(gv_user_limit, self).create(values)
+    
+class gv_config_settings(osv.osv.osv_memory):
+    _inherit = 'res.config.settings'
+    _name="gv.config.settings"
+    
+    userLimit = fields.Integer(string="Maximum No of Employees", help="Maximum number of active employees allowable for this installation. ")
+    
+    def get_default_userLimit(self, cr, uid, ids, context=None):
+        config_obj = self.pool.get('gv.config.settings')
+        config_ids = config_obj.search(cr, uid, [], limit=1, order='id DESC', context=context)
+        if config_ids:
+            config = config_obj.browse(cr, uid, config_ids[0], context=context)
+            return {'userLimit': config.userLimit}
+        return {}
+    
+ 
+    def set_default_userLimit(self, cr, uid, ids, context=None):
+        config_obj = self.pool.get('gv.config.settings')
+        config_ids = config_obj.search(cr, uid, [], limit=1, order='id DESC', context=context)
+        config = self.browse(cr, uid, ids[0], context)
+        config_obj.write(cr, uid, config_ids[0], {'userLimit': config.userLimit})
