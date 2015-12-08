314bee51ef05f0ffefb37c8d8ef3cf258f43595c
yihong <yihong@groventure.com>
2015-12-08 10:28:55 +0800

    add biz registration number to res.partners which will show when res.partner is a company

--- gv_reseller/models/project_template.py
+++ gv_reseller/models/project_template.py
@@ -9,9 +9,15 @@ Created on 27 Nov 2015
 class gv_project_template(models.Model):
     _inherit = 'project.project'
 
-    client_business_registration_no = fields.Char(string="Biz Registration No")
-    customer_remarks = fields.Text(string="Customer Remarks")
-    admin_remarks = fields.Text(string="Admin Remarks")
+    client_business_registration_no = fields.Char(string='Biz Registration No')
+    customer_remarks = fields.Text(string='Customer Remarks')
+    admin_remarks = fields.Text(string='Admin Remarks')
     sale_order = fields.Many2one('sale.order', string='Sale Order')
 
-        
+
+
+
+class gv_partner_template(models.Model):
+    _inherit = 'res.partner'
+    
+    business_registration_number = fields.Char(string='Business Registration')
