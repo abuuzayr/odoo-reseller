db0d0f2b626fcd1d59fd4fa3b981e6c17fa4cdcb
yihong <yihong@groventure.com>
2015-12-10 10:05:12 +0800

    add project filter

--- gv_reseller/static/js/project_filter.js
+++ gv_reseller/static/js/project_filter.js
@@ -1,17 +1,67 @@
 (function($){
+	var status_index;
+	var filter_status = "";
+	
 	$(document).ready(function() { 
-	  $('.gv_reseller-status-filter tr td').each(function(){
-	   $(this).click(function(){
-	      $('.gv_reseller-line-up-arrow').removeClass('gv_reseller-line-up-arrow')
-	      $(this).find('hr').addClass('gv_reseller-line-up-arrow')
-	   })
-	  })
+		
+		set_project_table_status_index();
+		$('.gv_reseller-status-filter tr td').each(function(){
+			$(this).click(function(){
+				$('.gv_reseller-line-up-arrow').removeClass('gv_reseller-line-up-arrow');
+				$(this).find('hr').addClass('gv_reseller-line-up-arrow');
+				$('#input-search-filter')[0].value = '';
+				filter_status = $(this)[0].getAttribute('value');
+				filter(filter_status);
+			})
+		})
 	  
-	  
-	  
-	  
-	  
-	  
-	  
+		$('#button-search-filter').click(function(){
+			filter(filter_status,  $('#input-search-filter')[0].value);
+		})
 	});
+	
+	function set_project_table_status_index(){
+		for (var j = 0, col; col = $('.gv_reseller-project-table tr')[0].cells[j]; j++) {
+			if (col.innerHTML.toLowerCase().contains('status')){
+				status_index = j;
+				return;
+			}
+		}
+	}
+		
+	function filter(status_string, search_string){
+		status_string = status_string || ''; //converts undefined to empty string
+		search_string = search_string || ''; //converts undefined to empty string
+		for (var i = 1, row; row = $('.gv_reseller-project-table')[0].rows[i]; i++) {
+			var match = compare_ignore_case(row.cells[status_index].innerHTML, status_string);
+			
+			if (match){
+				match = table_row_contains(row, search_string);
+			}
+			
+			if (match){
+				$(row).removeClass('hidden');
+			}
+			else {
+				$(row).addClass('hidden');
+			}
+		}
+	}
+	
+	function table_row_contains(table_row, search_string){
+		for (var j = 0, col; col = table_row.cells[j]; j++) {
+			if (compare_ignore_case(col.innerHTML, search_string)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	function compare_ignore_case(string, searchword){
+		if (string==null || searchword==null){
+			console.log('compare_ignore_case has a null value: ' + string + ' : ' + searchword);
+			return false;
+		}
+		return string.toString().toLowerCase().contains(searchword.toString().toLowerCase());
+	}
 })(jQuery);
